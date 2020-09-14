a2d6e4590015ca831c69ca0831fdc2f6fc41cdbf
harry <harry@atguigu.com>
2020-09-14 10:37:50 +0800

    测试综合使用，左边提交

--- pro06-atguigu-maven-module01/src/main/java/com/atguigu/git/HelloWorld.java
+++ pro06-atguigu-maven-module01/src/main/java/com/atguigu/git/HelloWorld.java
@@ -9,7 +9,7 @@ public class HelloWorld {
         System.out.println("Hello new content");
         System.out.println("Hello");// 测试冲突，在master分支所做修改
         System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello");// 测试综合使用，左边提交
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
