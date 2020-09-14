16edd944299bc81a63d5e3f28c7afcad83ef3213
harry <harry@atguigu.com>
2020-09-14 09:54:11 +0800

    测试冲突，在hotfix分支所做修改

--- pro06-atguigu-maven-module01/src/main/java/com/atguigu/git/HelloWorld.java
+++ pro06-atguigu-maven-module01/src/main/java/com/atguigu/git/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello");// 测试冲突，在hotfix分支所做修改
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
