526a81b3e7b472653e1cab09821c4a5d968c4338
harry <harry@atguigu.com>
2020-09-14 09:49:20 +0800

    创建了一个HelloWorld类

---
+++ pro06-atguigu-maven-module01/src/main/java/com/atguigu/git/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.atguigu.git;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+
+}
